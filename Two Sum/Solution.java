03982e3ae22f7d51594b1a294807835d3d7b8e46
EkkaKEA <canabis220@mail.ru>
2024-11-10 11:04:57 +0300

    решение

---
+++ Two Sum/Solution.java
@@ -0,0 +1,18 @@
+﻿class Solution {
+    public int[] twoSum(int[] nums, int target) {
+          // Проходим по каждому числу в массиве
+        for (int i = 0; i < nums.length; i++) {
+            // Для каждого числа ищем пару, с которой сумма равна target
+            for (int j = i + 1; j < nums.length; j++) {
+                // Если сумма двух чисел равна target, возвращаем их индексы
+                if (nums[i] + nums[j] == target) {
+                    return new int[] { i, j };
+                }
+            }
+        }
+        
+        // Если решения нет, выбрасываем исключение (по условиям задачи решение всегда существует)
+        throw new IllegalArgumentException("No solution found");
+      
+    }
+}
